110e80ecd1af576b77b71c15d7fd1095f180fe06
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2025-11-03 15:08:32 +0100

    consolidated zod validation for forms

--- src/app/(app)/studies/queries/getStudyParticipants.ts
+++ src/app/(app)/studies/queries/getStudyParticipants.ts
@@ -1,17 +1,13 @@
 import db from "db"
 import { resolver } from "@blitzjs/rpc"
-import { z } from "zod"
 import { Prisma } from "db"
+import { GetStudyParticipants } from "../validations"
 
 export const participantWithEmail = Prisma.validator<Prisma.ParticipantStudyDefaultArgs>()({
   include: { user: { select: { email: true } } },
 })
 
 export type ParticipantWithEmail = Prisma.ParticipantStudyGetPayload<typeof participantWithEmail>
-
-const GetStudyParticipants = z.object({
-  studyId: z.number(),
-})
 
 export default resolver.pipe(
   resolver.zod(GetStudyParticipants),

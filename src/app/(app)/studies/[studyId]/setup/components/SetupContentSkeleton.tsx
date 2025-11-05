d776ba0cd1f79428a2df5aab6402a08dca1f23c7
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2025-11-05 19:56:39 +0100

    removed steps from setup skeleton

--- src/app/(app)/studies/[studyId]/setup/components/SetupContentSkeleton.tsx
+++ src/app/(app)/studies/[studyId]/setup/components/SetupContentSkeleton.tsx
@@ -1,13 +1,26 @@
 export default function SetupContentSkeleton() {
   return (
     <div className="animate-pulse space-y-4">
-      <div className="h-6 w-1/3 bg-gray-300 rounded mx-auto"></div>
-      <div className="space-y-3">
-        <div className="h-4 w-full bg-gray-300 rounded"></div>
-        <div className="h-4 w-5/6 bg-gray-300 rounded"></div>
-        <div className="h-4 w-4/6 bg-gray-300 rounded"></div>
+      {/* Form fields skeleton */}
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <div className="h-4 w-24 bg-gray-300 rounded"></div>
+          <div className="h-10 w-full bg-gray-300 rounded"></div>
+        </div>
+        <div className="space-y-2">
+          <div className="h-4 w-32 bg-gray-300 rounded"></div>
+          <div className="h-10 w-full bg-gray-300 rounded"></div>
+        </div>
+        <div className="space-y-2">
+          <div className="h-4 w-28 bg-gray-300 rounded"></div>
+          <div className="h-24 w-full bg-gray-300 rounded"></div>
+        </div>
       </div>
-      <div className="h-10 w-32 bg-gray-300 rounded"></div>
+      {/* Action buttons skeleton */}
+      <div className="flex justify-end gap-2 mt-6">
+        <div className="h-10 w-24 bg-gray-300 rounded"></div>
+        <div className="h-10 w-32 bg-gray-300 rounded"></div>
+      </div>
     </div>
   )
 }
